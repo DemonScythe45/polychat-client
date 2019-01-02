98aaae3053643bd2967667d7da5d2bcd160fbf5e
DemonScythe45 <crafterguy1701@gmail.com>
2019-01-02 16:41:44 -0700

    Added automatic reconnect function

--- src/main/java/club/moddedminecraft/polychat/client/ModClass.java
+++ src/main/java/club/moddedminecraft/polychat/client/ModClass.java
@@ -24,6 +24,7 @@ import club.moddedminecraft.polychat.networking.io.MessageBus;
 import club.moddedminecraft.polychat.networking.io.ServerInfoMessage;
 import club.moddedminecraft.polychat.networking.io.ServerStatusMessage;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -39,6 +40,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Map;
 import java.util.Properties;
 
@@ -50,9 +52,12 @@ public class ModClass
     public static final String VERSION = "1.0";
     //Used to determine whether the server cleanly shutdown or crashed
     public static boolean shutdownClean = false;
+    //Used to determine whether to send a connection lost warning in game
+    public static boolean isConnected = true;
     public static MinecraftServer server;
     public static Properties properties;
-    public static MessageBus messageBus;
+    public static MessageBus messageBus = null;
+    public static Thread reattachThread;
 
     //Forces the server to allow clients to join without the mod installed on their client
     @NetworkCheckHandler
@@ -64,14 +69,48 @@ public class ModClass
     public void preInit(FMLPreInitializationEvent event) {
         //Registers game event listener class
         MinecraftForge.EVENT_BUS.register(new EventListener());
+        reattachThread = new Thread(this::reattachThread);
         //Sets up the config values
         handleConfiguration(event.getModConfigurationDirectory());
         //Registers the shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread(this::shutdownHook));
     }
 
+    //Reattaches to the main server should it restart
+    public void reattachThread() {
+        try {
+            while (true) {
+                try  {
+                    Thread.sleep(1000);
+                    if (messageBus == null || (messageBus.isSocketClosed())) {
+                        if (isConnected) {
+                            isConnected = false;
+                            EventListener.sendTextComponent(new TextComponentString("[PolyChat] Lost connection to main server, attempting reconnect..."));
+                        }
+                        if (messageBus != null) messageBus.stop();
+                        messageBus = new MessageBus(new Socket(properties.getProperty("address"), Integer.parseInt(properties.getProperty("port"))), EventListener::handleMessage);
+                        messageBus.start();
+                        if (!messageBus.isSocketClosed()) {
+                            Thread.sleep(2000);
+                            sendServerOnline();
+                            EventListener.sendTextComponent(new TextComponentString("[PolyChat] Connection re-established!"));
+                            isConnected = true;
+                        }
+                    }
+                } catch (UnknownHostException e) {
+                    System.out.println("Unknown host exception on reattach");
+                } catch (IOException e) {
+                    System.out.println("IOException on reattach");
+                }
+            }
+        }catch (InterruptedException ignored) {
+            System.out.println("Reattach interrupted, stopping...");
+        }
+    }
+
     //Makes sure that the server offline message gets sent
     public void shutdownHook() {
+        reattachThread.interrupt();
         short exitVal;
         if (shutdownClean) {
             exitVal = 2;
@@ -79,6 +118,23 @@ public class ModClass
             exitVal = 3;
         }
         ServerStatusMessage statusMessage = new ServerStatusMessage(properties.getProperty("server_id"), exitVal);
+        ModClass.sendMessage(statusMessage);
+        try {
+            //Makes sure message has time to send
+            Thread.sleep(1000);
+        } catch (InterruptedException ignored) {}
+        messageBus.stop();
+    }
+
+    //Put in a method to be used in two places
+    public void sendServerOnline() {
+        //Reports the server as starting
+        ServerInfoMessage infoMessage = new ServerInfoMessage(properties.getProperty("server_id"),
+                properties.getProperty("server_name"),
+                properties.getProperty("server_address"), server.getMaxPlayers());
+        ModClass.sendMessage(infoMessage);
+        //Reports the server as online and ready to receive players
+        ServerStatusMessage statusMessage = new ServerStatusMessage(properties.getProperty("server_id"), (short) 1);
         ModClass.sendMessage(statusMessage);
     }
 
@@ -98,14 +154,8 @@ public class ModClass
     public void onStarted(FMLServerStartedEvent event) {
         //Connects to the main polychat server
         handleClientConnection();
-        //Reports the server as starting
-        ServerInfoMessage infoMessage = new ServerInfoMessage(properties.getProperty("server_id"),
-                properties.getProperty("server_name"),
-                properties.getProperty("server_address"), server.getMaxPlayers());
-        ModClass.sendMessage(infoMessage);
-        //Reports the server as online and ready to receive players
-        ServerStatusMessage statusMessage = new ServerStatusMessage(properties.getProperty("server_id"), (short) 1);
-        ModClass.sendMessage(statusMessage);
+        reattachThread.start();
+        sendServerOnline();
     }
 
     @EventHandler
